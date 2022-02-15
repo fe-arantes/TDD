20f56d7e7a92ff6a0d9edd4c67d388d1bc7b28cb
Felipe Arantes <58771056+fe-arantes@users.noreply.github.com>
2022-02-15 10:19:58 -0300

    Update

---
+++ LeilaoOnline/LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using LeilaoOnline.Core.Entidade;
+
+var leilao = new Leilao("Van Gogh");
+var fulano = new Interessada("Fulano", leilao);
+var maria = new Interessada("Maria", leilao);
+
+leilao.RecebeLance(fulano, 800);
+leilao.RecebeLance(maria, 900);
+leilao.RecebeLance(fulano, 1000);
+
+leilao.TerminaPregao();
+
+Console.WriteLine(leilao.Ganhador.Valor);
+Console.ReadKey();
